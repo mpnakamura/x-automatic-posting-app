aa135f4582ace1dca690a4554abf83e6c3ae2514
mpnakamura <sny01.mzk10.lv@gmail.com>
2023-12-02 02:26:25 +0900

    ber

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def create_app():
     uri = os.getenv("DATABASE_URL")  # HerokuのデータベースURLを取得
     if uri.startswith("postgres://"):
       uri = uri.replace("postgres://", "postgresql://", 1)
+      
     app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
     db.init_app(app)
